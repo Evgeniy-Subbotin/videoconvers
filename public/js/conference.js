3c4868b2b0d5f8ab1e75e77f1012f67463c9bb16
Сафаров Эльшад Нурали оглы <elshad.safarov@domru.ru>
2021-03-08 19:06:38 +0500

    Port changed

--- public/js/conference.js
+++ public/js/conference.js
@@ -8,7 +8,7 @@ const videoGrid = $('#video-grid');
 const myPeer = new Peer(undefined, {
     path: '/peerjs',
     host: '/',
-    port: '3000'
+    port: '443'
 });
 let localMediaStream;
 let userMedia;
